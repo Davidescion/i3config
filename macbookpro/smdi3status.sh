22212723edfbba4eae63c070696206bc9a4bec0f
Davide <davidescion@gmail.com>
2018-08-16 06:53:31 +0200

    has keybaord backlight

---
+++ macbookpro/smdi3status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#this is a pretty stupid program made in bash to practice the use of awk and grep because yah know what else should I do
+#gud shit happens here
+
+
+while true
+do
+mem=$(free --mega | grep Mem | awk '{print $3, "/ " $2}') #get used and total mem
+kernelver=$(uname -a | awk '{print $3}') #this is the easiest to understand, we just get the kernel version from uname -a
+date=$(date | awk '{print $4, $3, $2, $6}') #cmon do I really need to explain this?
+floatCPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}') #bash insanity to get cpu usage
+CPU=$(printf "%.*f\n" 2 $floatCPU)
+floatKB=$(cat /sys/class/leds/smc\:\:kbd_backlight/brightness | awk '{usage=($1/255)*100} END {print usage}')
+KB=$(printf "%.*f\n" 2 $floatKB)
+lol=$(echo "Keyboard:" $KB "%" "|" "CPU:" $CPU "% |" "Used Memory:" $mem "MB" "|" "Kernel:" $kernelver "|" "Time:" $date)
+echo -n $lol
+sleep 1
+done
