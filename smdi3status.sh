4eec58be429e513c18a293adbbfe267286840353
Davide <davidescion@gmail.com>
2018-09-04 18:00:10 +0000

    Now with a 100% more working cpu usage

--- smdi3status.sh
+++ smdi3status.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-#this is a pretty stupid program made in bash to practice the use of awk and grep because yah know what else should I do
 #gud shit happens here
-while true
-do
+while true #start a loop
+do #do things inside the loop
 mem=$(free --mega | grep Mem | awk '{print $3, "/", $2, "MB"}') #get used and total mem
 kernelver=$(uname -a | awk '{print $3}') #this is the easiest to understand, we just get the kernel version from uname -a
-date=$(date | awk '{print $4, $3, $2, $6}') #cmon do I really need to explain this?
-floatCPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}') #bash insanity to get cpu usage
-CPU=$(printf "%.*f\n" 2 $floatCPU)
-rootspace=$(df -h | grep sda2 | awk '{print $4, "free in", $6}')
-lol=$(echo $CPU "%" "|" $mem "|" $rootspace "|" $kernelver "|"  $date)
-echo -n $lol
-sleep 1
-done
-# TODO
-# fix % in lol, not so clean of a solution, but adding 1 more variable uses more ram.
+date=$(TZ='UTC' date | awk '{print $4, $3, $2, $6}') #cmon do I really need to explain this? NOTE THAT YOU NEED TO PUT YOUR TIMEZONE
+CPU=$(top -bn 1 | grep "%Cpu(s)" | awk '{usage=100-$8} END {print usage}') #gets gpu usage from top
+rootspace=$(df -h | grep sdb2 | awk '{print $4, "free in", $6}') #gets free space from drive YOU PROBABLY NEED TO PUT SDXY WHERE SDXY IS YOUR /
+lol=$(echo $CPU "%" "|" $mem "|" $rootspace "|" $kernelver "|"  $date) #make a nice var with all of the stuff above
+echo -n $lol #print it
+sleep 1 #sleep for 1 second to save dat cpu powa
+done #stop doing things inside the loop
